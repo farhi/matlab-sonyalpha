535c286ef54626908eb406b7cdbfaa68fb9be90f
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2018-02-19 00:00:57 +0100

    sony alpha for Matlab initial commit that is functional

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # matlab-sonyalpha
 Control a Sony Alpha Camera
+
+You need to start the PlayMemories Remote Control App on the camera. The Camera should be connected using an ad-hoc network, which SSID is shown on the camera screen. The Camera IP will then be:
+
+http://192.168.122.1:8080
+
+Then you can send/receive commands such as:
+
+**getApplicationInfo**: request modes:
+
+- curl -d "{'method': 'getApplicationInfo','params': [],'id': 1,'version': '1.0'}"  http://192.168.122.1:8080/sony/camera; echo ''
+- {'result':['Smart Remote Control SR\/4.30 __SAK__','2.1.4'],'id':1}
+
+**getVersions**: get versions:
+
+- curl -d "{'method': 'getVersions','params': [],'id': 1,'version': '1.0'}"  http://192.168.122.1:8080/sony/camera; echo ''
+- {'result':[['1.0','1.1','1.2','1.3','1.4']],'id':1}
+
+**getAvailableApiList**:
+- curl -d "{'method': 'getAvailableApiList','params': [],'id': 1,'version': '1.0'}"  http://192.168.122.1:8080/sony/camera; echo ''
+- {'result':[['getVersions','getMethodTypes','getApplicationInfo','getAvailableApiList','getEvent','startRecMode','stopRecMode']],'id':1}
+
+**startRecMode**: set camera in rec mode (shoot)
+
+- curl -d "{'method': 'startRecMode','params': [],'id': 1,'version': '1.0'}"  http://192.168.122.1:8080/sony/camera; echo ''
+- {'result':[0],'id':1}
+
+**livestream**:
+
+- curl -d '{"method": "startLiveview","params": [],"id": 1,"version": "1.0"}'  http://192.168.122.1:8080/sony/camera; echo ''
+- http://192.168.122.1:8080/liveview/liveviewstream
+
+**then view livestream with**: https://github.com/micolous/gst-plugins-sonyalpha
+
+- gst-launch-1.0 souphttpsrc location=http://192.168.122.1:8080/liveview/liveviewstream ! sonyalphademux ! jpegparse ! jpegdec ! videoconvert ! autovideosink
+
+**capture a frame** with ffmpeg (takes 2s), can be put into background
+
+- ffmpeg  -ss 1 -i http://192.168.122.1:8080/liveview/liveviewstream -frames:v 1 thumbnail.png
+- https://github.com/abarbu/ffmpeg-matlab use Matlab hook, not faster, takes also 2 s but can not be put in background
+
+
+Requirements
+------------
+
+- ffmpeg
+- curl
+
+
+
---
+++ sonyalpha.m
@@ -0,0 +1,320 @@
+classdef sonyalpha < handle
+  % see https://github.com/micolous/gst-plugins-sonyalpha
+  % pysony: https://github.com/Bloodevil/sony_camera_api
+  %
+  % Camera Remote API by Sony
+
+  properties
+    url           = 'http://192.168.122.1:8080/sony/camera';
+    exposureMode  = '';
+    cameraStatus  = '';
+    selfTimer     = '';
+    zoomPosition  = '';
+    shootMode     = '';
+    exposureCompensation = ''; % in EV
+    fNumber       = '';
+    focusMode     = '';
+    isoSpeedRate  = '';
+    shutterSpeed  = '';
+    whiteBalance  = '';
+    status        = '';
+  end % properties
+  
+  methods
+    function self = sonyalpha(url)
+      % sonyalpha: initialize the remote control for Sony Alpha Camera
+      if nargin > 1
+        self.url = url;
+      end
+
+      self.startRecMode;
+      self.getstatus;
+      
+    end % sonyalpha
+    
+    % INIT and INFO stuff
+    function status = getstatus(self)
+      % getstatus: get the Camera status and all settings
+      json = '{"method": "getEvent", "params": [false], "id": 1, "version": "1.2"}';
+      [ret, message] = curl(self.url, json);
+      message = loadjson(message);
+      message = message.result;
+      status = struct();
+      status.unsorted ={};
+      for index=1:numel(message)
+        this = message{index};
+        if isempty(this), continue; end
+        if isfield(this, 'type') && isfield(this, this.type)
+          status.(this.type) = this.(this.type);
+          if iscell(status.(this.type)) && numel(status.(this.type)) == 1
+            status.(this.type) = status.(this.type){1};
+          end
+        elseif isfield(this, 'type')
+          if iscell(this) && numel(this) == 1
+            this = this{1};
+          end
+          status.(this.type) = this;
+        else
+          status.unsorted{end+1} = this;
+        end
+      end
+      
+      % store that information into properties
+      self.status       = status;
+      try
+        self.exposureMode = status.exposureMode.currentExposureMode;
+        self.cameraStatus = status.cameraStatus;
+        self.selfTimer    = status.selfTimer.currentSelfTimer;
+        self.zoomPosition = status.zoomInformation.zoomPosition;
+        self.shootMode    = status.shootMode.currentShootMode;
+        self.exposureCompensation = status.exposureCompensation.currentExposureCompensation/3;
+        self.fNumber      = status.fNumber.currentFNumber;
+        self.focusMode    = status.focusMode.currentFocusMode;
+        self.isoSpeedRate = status.isoSpeedRate.currentIsoSpeedRate;
+        self.shutterSpeed = status.shutterSpeed.currentShutterSpeed;
+        self.whiteBalance = status.whiteBalance.currentWhiteBalanceMode;
+      end
+    end
+    
+    function ret=getApplicationInfo(self)
+      ret = self.get('getApplicationInfo');
+    end
+    
+    function ret=getAvailableApiList(self)
+      ret = self.get('getAvailableApiList');
+    end
+    
+    function ret=getVersions(self)
+      ret = self.get('getVersions');
+    end
+    
+    % generic get for most API commands ------------------------------------
+    function ret=get(self, getAPI)
+      % getAPI can be any  Sony API call without argument
+      %
+      % getApplicationInfo
+      % getAvailableApiList
+      % getVersions
+      %
+      % startRecMode
+      % actTakePicture
+      % getAvailableExposureMode
+      % getAvailableFocusMode
+      % getAvailableSelfTimer
+      % getAvailableExposureCompensation
+      % getAvailableFNumber
+      % getAvailableShutterSpeed
+      % getWhiteBalance
+      % getIsoSpeedRate
+      % getAvailablePostviewImageSize
+      json = [ '{"method": "' getAPI '","params": [],"id": 1,"version": "1.0"}' ];
+      ret  = curl(self.url, json);
+    end
+    
+    function ret=set(self, getAPI, value)
+      % 'setSelfTimer',    (0, 2 or 10)
+      % 'setExposureMode', ('Program Auto','Aperture', 'Shutter', 'Manual', 'Intelligent Auto')
+      % 'setFocusMode',    ('AF-S'    'AF-C'    'DMF'    'MF')
+      % 'setExposureCompensation', (-9:9) per 1/3 EV
+      % 'setFNumber',      (e.g. '3.5')
+      % 'setShutterSpeed'  (e.g. '1/15')
+      % 'setIsoSpeedRate'  ('AUTO'    '100'    '200'    '400'    '800'    '1600'    '3200'    '6400'    '12800'  '25600')
+      % 'setPostviewImageSize' ('Original'    '2M') <- faster image transfer
+      if isnumeric(value),  value = num2str(value);
+      elseif ischar(value), value = [ '"' value '"' ];
+      elseif islogical(value)
+        if value, value = 'true'; else value = 'false'; end
+      end
+      json = [ '{"method": "' getAPI '","params": [' value '],"id": 1,"version": "1.0"}' ];
+      ret  = curl(self.url, json);
+    end
+    
+    % Camera Shooting ----------------------------------------------------------
+    function ret=startRecMode(self)
+      ret = self.get('startRecMode');
+    end
+    
+    function ret=close(self)
+      ret=self.get('stopRecMode');
+    end
+    
+    function url=actTakePicture(self)
+      url = self.get('actTakePicture');
+    end
+    
+    % upper level camera shooting and display
+    function [im, exif] = imread(self)
+    
+      url  = self.actTakePicture;
+      % get the extension
+      [p, f, e] = fileparts(char(url));
+      
+      % then get URL and display it
+      file = [ tempname e ]
+      im   = urlwrite(char(url), file);
+      im   = imread(im);
+      if nargout > 1, exif = imfinfo(file); end
+      delete(file);
+     
+    end % imread
+    
+    function h = image(self)
+      h = image(self.imread);
+    end
+    
+    function h = plot(self)
+      h = image(self.imread);
+    end
+    
+    % Camera settings ----------------------------------------------------------
+    function ret = iso(self, value)
+      % iso(s):        get the ISO setting as a string (can be 'AUTO')
+      % iso(s, 'iso'): set the ISO setting as a string (can be 'AUTO')
+      %
+      % The ISO value can be e.g. AUTO 100 200 400 800 1600 3200 6400 12800 25600 
+      if nargin < 2, value = ''; end
+      if isempty(value)
+        ret = get(self, 'getIsoSpeedRate');
+      else
+        ret = self.set('setIsoSpeedRate', num2str(value));
+      end
+    end
+    
+    function ret = mode(self, value)
+      % mode(s):         get the shooting Mode (e.g. PASM)
+      % mode(s, 'PASM'): set the shooting Mode (e.g. PASM) as a string
+      %
+      % The shooting Mode can be 'Program Auto', 'Aperture', 'Shutter', 'Manual'
+      %   'Intelligent Auto', or 'P', 'A', 'S', 'M'
+      if nargin < 2, value = ''; end
+      if isempty(value)
+        ret = get(self, 'getExposureMode');
+        switch ret
+        case 'Program Auto'; ret = 'P';
+        case 'Aperture';     ret = 'A';
+        case 'Shutter';      ret = 'S';
+        case 'Manual';       ret = 'M';
+        end
+      else
+        switch upper(value(1))
+        case 'P'; value = 'Program Auto';
+        case 'A'; value = 'Aperture';
+        case 'S'; value = 'Shutter';
+        case 'M'; value = 'Manual';
+        end
+        ret = self.set('setExposureMode', value);
+      end
+    end
+    
+    function ret = timer(self, value)
+      % timer(s):      get the self Timer setting
+      % timer(s, val): set the self Timer setting in seconds
+      %
+      % The self Timer value can be e.g. 0, 2 or 10 (numeric)
+      if nargin < 2, value = ''; end
+      if isempty(value)
+        ret = get(self, 'getSelfTimer');
+      else
+        ret = self.set('setSelfTimer', num2str(value));
+      end
+    end
+    
+    function ret = shutter(self, value)
+      % shutter(s):      get the shutter speed setting (S mode)
+      % shutter(s, val): set the shutter speed setting (S mode) as a string
+      %
+      % The shutter speed value can be e.g. '30"', '1"', '1/2', '1/30', '1/250' (string)
+      %   where the " symbol stands for seconds.
+      if nargin < 2, value = ''; end
+      if isempty(value)
+        ret = get(self, 'getShutterSpeed');
+      else
+        ret = self.set('setShutterSpeed', num2str(value));
+      end
+    end
+    
+    function ret = fnumber(self, value)
+      % fnumber(s):      get the F/D number (apperture) setting (A mode)
+      % fnumber(s, val): set the F/D number (apperture) setting (A mode) as a string
+      %
+      % The F/D number value can be e.g. '1.4','2.0','2.8','4.0','5.6'
+      if nargin < 2, value = ''; end
+      if isempty(value)
+        ret = get(self, 'getFNumber');
+      else
+        ret = self.set('setFNumber', num2str(value));
+      end
+    end
+    
+    function ret = white(self, value)
+      % white(s):      get the white balance setting
+      % white(s, val): set the white balance setting
+      %
+      % The white balance can be a string such as 
+      %  'Auto WB'
+      %  'Daylight'
+      %  'Shade' 
+      %  'Cloudy' 
+      %  'Incandescent' 
+      %  'Fluorescent: Warm White (-1)' 
+      %  'Fluorescent: Cool White (0)'
+      %  'Fluorescent: Day White (+1)'
+      %  'Fluorescent: Daylight (+2)'
+      %  'Flash'
+      % or a Temperature (numeric for 'Color Temperature' mode) in 2500-9900 K
+      if nargin < 2, value = ''; end
+      if isempty(value)
+        ret = get(self, 'getWhiteBalance');
+      else
+        if ischar(value)
+          json = [ '{"method": "setWhiteBalance","params": ["' value '", false, -1],"id": 1,"version": "1.0"}' ];
+          ret = curl(self.url, json);
+        elseif isnumeric(value)
+          value = round(value/100)*100;
+          json = [ '{"method": "setWhiteBalance","params": ["Color Temperature", true, ' num2str(value) '],"id": 1,"version": "1.0"}' ];
+          ret = curl(self.url, json);
+        end
+      end
+    end
+    
+    
+  end % methods
+  
+end
+
+% internal communication done with curl ----------------------------------------
+
+function message = curl(url, post)
+  % prepare curl command
+  if ismac,      precmd = 'DYLD_LIBRARY_PATH= ;';
+  elseif isunix, precmd = 'LD_LIBRARY_PATH= ; '; 
+  else           precmd=''; end
+  
+  cmd = [ 'curl -d ''' post ''' ' url ];
+  
+  % evaluate command
+  [ret, message]=system([ precmd  cmd ]);
+  
+  if ret % error
+    disp(cmd)
+    error(message);
+  end
+  
+  % decode JSON output into struct
+  message = loadjson(message); % We use JSONlab reader which is more robust
+
+  if isstruct(message) && isfield(message, 'result') && ischar(message.result)
+    message.result = strrep(message.result', '\/','/');
+  end
+  if isstruct(message) && numel(fieldnames(message)) == 2
+    if  isfield(message, 'result') && isfield(message, 'id') && message.id == 1
+      message = message.result;
+    elseif isfield(message, 'error')
+      message = message.error;
+    end
+  end
+  if iscell(message) && numel(message) == 1
+    message = message{1};
+  end
+end
+
